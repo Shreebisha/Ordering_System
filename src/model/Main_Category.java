d6cafb7018f04c586051d471cad423a794f6c8ff
Shreebisha <shreebisha23stha@gmail.com>
2023-01-27 19:00:06 +0545

    Initial commit

---
+++ src/model/Main_Category.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Main_Category {
+    public Main_Category(String name) {
+        this.name = name;
+    }
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
